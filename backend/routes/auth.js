26f7fd198205765436a165057c66bebdc5560dc2
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-09-13 17:10:43 +0530

    Created a Login Endpoint and sending an auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,7 @@ const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = 'Ansul$is$a$goo$d$bo$y';
+const JWT_SECRET = 'Ansul$is$a$goo$d$bo$y'; // json webtoken Secret key value
 
 // Create a User using: POST "/api/auth/createuser". No login required
 router.post('/createuser', [
@@ -16,12 +16,12 @@ router.post('/createuser', [
    body('email', 'Enter a valid email').isEmail(),
    // password must be at least 8 chars long
    body('password', 'Password must be atleast 8 characters').isLength({ min: 8 }),
-
 ], async (req, res) => {
 
    // Finds the validation errors in this request and wraps them in an object with handy functions
-   //If there are errors, return Bad request and the errors
    const errors = validationResult(req);
+
+   //If there are errors, return Bad request and the errors
    if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
    }
@@ -65,14 +65,60 @@ router.post('/createuser', [
          }
       }
       const authtoken = jwt.sign(data, JWT_SECRET);
-      console.log(jwtData);
+      // console.log(jwtData);
 
       // res.json(user);
       res.json({ authtoken });
 
    } catch (error) {
       console.log(error.message);
-      res.status(500).send("Some Error occured");
+      res.status(500).send("Internal Server Error");
+   }
+})
+
+
+// Authenticate a user using : POST "/api/auth/login". No login required
+router.post('/login', [
+
+   // email must be an email
+   body('email', 'Enter a valid email').isEmail(),
+   // password must be at least 8 chars long
+   body('password', 'Password cannot be blank').exists(),
+
+], async (req, res) => {
+   // Finds the validation errors in this request and wraps them in an object with handy functions
+   const errors = validationResult(req);
+
+   //If there are errors, return Bad request and the errors
+   if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+   }
+
+   // taking email and password from the body which is send by user.
+   const { email, password } = req.body;
+
+   try {
+      let user = await User.findOne({ email });
+      if (!user) {
+         return res.status(400).json({ error: "Please try to login with correct credentials." });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+         return res.status(400).json({ error: "Please try to login with correct credentials." });
+      }
+
+      const data = {
+         user: {
+            id: user.id
+         }
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+
+   } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server Error");
    }
 })
 
