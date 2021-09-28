1b16a2e9d85e1cf2472a098b438bc3b2b982cdea
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-09-28 13:51:17 +0530

    Adding font awesome icons.  Created Alert component.

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -7,6 +7,10 @@ const NoteItem = (props) => {
             <div className="card-body">
                <h5 className="card-title">{note.title}</h5>
                <p className="card-text">{note.description}</p>
+               <div className="d-flex justify-content-between">
+                  <i className="fas fa-trash-alt"></i>
+                  <i className="fas fa-edit"></i>
+               </div>
             </div>
          </div>
       </div>
