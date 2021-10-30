51f84bec95fe41da5291fa98250814702066ffe9
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-10-30 18:45:19 +0530

    Change database from localhost to altas

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+// const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+const mongoURI = "mongodb+srv://AnsulAgrawal:ansul9@inotebook-cluster.cqmdt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const connectToMongo = () => {
    mongoose.connect(mongoURI, () => {
